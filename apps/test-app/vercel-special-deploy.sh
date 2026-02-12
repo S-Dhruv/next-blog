ec5871c9e58eafd5f53bf7aed0d3010566ebb207
dhruvsshah171 <dhruvsshah171@gmail.com>
2026-02-13 01:07:43 +0530

    Changes install config 2

--- apps/test-app/vercel-special-deploy.sh
+++ apps/test-app/vercel-special-deploy.sh
@@ -1,6 +1,6 @@
 NEXT_BLOG_DATA_PATH=/tmp
-bun --cwd=../.. run build
 bun --cwd=../.. i
+bun --cwd=../.. run build
 mkdir -p "public/api/next-blog/dashboard/static/"
 cp -r ../../packages/core/dist/nextjs/assets/@supergrowthai/next-blog-dashboard/static/* ./public/api/next-blog/dashboard/static/
 bun build-plugins.ts
