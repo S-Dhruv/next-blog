e7faa263a01c32d7f6993e42ca291bdd9ec1112b
dhruvsshah171 <dhruvsshah171@gmail.com>
2026-02-13 12:54:42 +0530

    AI change

--- apps/test-app/next.config.js
+++ apps/test-app/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    transpilePackages: ['@supergrowthai/next-blog', '@supergrowthai/next-blog-types', '@supergrowthai/oneapi', '@supergrowthai/next-blog-ui', '@supergrowthai/plugin-dev-kit']
+    transpilePackages: [
+        '@supergrowthai/next-blog', 
+        '@supergrowthai/next-blog-types', 
+        '@supergrowthai/oneapi', 
+        '@supergrowthai/next-blog-ui', 
+        '@supergrowthai/plugin-dev-kit',
+        '@supergrowthai/utils',
+        '@supergrowthai/mq',
+        '@supergrowthai/tq'
+    ]
 };
 
 module.exports = nextConfig;
